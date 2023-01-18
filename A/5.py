7e455fdbd08ac525da0dd4851ffa1d4e43370caa
elihu26 <evaldelo22@jaumebalmes.net>
2023-01-18 20:43:56 +0100

    Practica 9 A

---
+++ A/5.py
@@ -0,0 +1,3 @@
+areas = [“cuina”, 7.88, “menjador”, 13.0, “terrassa”, 20.34, “lavabo”, 6.55, “habitació1”, 7.98, “habitació2”, 12, “rebedor”, 4.23]
+print(areas[0])
+print(areas[13])
---
+++ A/sum.py
@@ -0,0 +1,5 @@
+#imprimir la suma de 3 i 7
+n1 = 3
+n2 = 7
+suma = n1 + n2
+print(suma)
