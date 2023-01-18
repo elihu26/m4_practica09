411407d6a2b8a7a49c5d8333cad53faaff97ea40
david <dares22@jaumebalmes.net>
2023-01-18 20:46:02 +0100

    Practicas de david B

---
+++ B/conc.py
@@ -0,0 +1,10 @@
+
+#Imprimir el resultat de la suma de les dues variables (int) (comentari a afegir al codi)
+
+#Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)(comentari a afegir al codi)
+
+num1 = 10
+num2 = 5
+suma = num1 + num2
+print(suma)
+print("La suma de las 2 variables es:", suma)
---
+++ B/invest.py
@@ -0,0 +1,4 @@
+
+#Imprimir el total dels diners acumulats després de 7 anys (comentari a afegir al codi)
+
+print((100*1.1)*7)
